39ef77c02156362b2f83253d9e5ffa62e9314288
Joel Rose <mail@joel-rose.de>
2022-05-17 13:28:48 +0200

    feat: add code coverage calculation

--- index.js
+++ index.js
@@ -1,21 +1,69 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const minimatch = require('minimatch');
+const parse = require('lcov-parse');
+const fs = require('fs');
 
+function run() {
+  const lcovPath = core.getInput('path');
+  const excluded = core.getInput('exclude');
+  const excludedFiles = excluded.split(' ');
 
-// most @actions toolkit packages have async methods
-async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
+  if (!canParse(lcovPath)) {
+    return;
   }
+
+  parse(lcovPath, (_, data) => {
+    if (typeof data === 'undefined') {
+      core.setFailed('parsing error!');
+      return;
+    }
+
+    const linesMissingCoverage = [];
+
+    let totalFinds = 0;
+    let totalHits = 0;
+    data.forEach((element) => {
+      if (shouldCalculateCoverageForFile(element['file'], excludedFiles)) {
+        totalFinds += element['lines']['found'];
+        totalHits += element['lines']['hit'];
+
+        for (const lineDetails of element['lines']['details']) {
+          const hits = lineDetails['hit'];
+
+          if (hits === 0) {
+            const fileName = element['file'];
+            const lineNumber = lineDetails['line'];
+            linesMissingCoverage[fileName] =
+              linesMissingCoverage[fileName] || [];
+            linesMissingCoverage[fileName].push(lineNumber);
+          }
+        }
+      }
+    });
+    const coverage = (totalHits / totalFinds) * 100;
+    
+    core.setOutput('code_coverage', coverage);
+  });
+}
+
+function shouldCalculateCoverageForFile(fileName, excludedFiles) {
+  for (let i = 0; i < excludedFiles.length; i++) {
+    const isExcluded = minimatch(fileName, excludedFiles[i]);
+    if (isExcluded) {
+      core.debug(`Excluding ${fileName} from coverage`);
+      return false;
+    }
+  }
+  return true;
+}
+
+function canParse(path) {
+  if (fs.existsSync(path) && fs.readFileSync(path).length === 0) {
+    core.setFailed('lcov is empty!');
+    return false;
+  }
+
+  return true;
 }
 
 run();
